f7f59743bb0dd0bf78d206db8dd781eb5a87ae5a
Kazuki Sawada <kazuki@6715.jp>
2018-12-21 01:35:23 +0900

    add KaTeX

---
+++ modules/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import katex from "katex";
+import "katex/dist/katex.css";
+
+const renderMath = (src, dst, displayMode) => {
+	dst.insertAdjacentHTML("afterend", katex.renderToString(src.textContent.replace(/^[$\s]+|[$\s]+$/g, ""), {displayMode}));
+	dst.remove();
+};
+
+window.addEventListener("load", () => {
+	document.querySelectorAll("pre > code.language-math").forEach(elem => {
+		renderMath(elem, elem.parentElement, true);
+	});
+	document.querySelectorAll("code").forEach(elem => {
+		if (/^\s*\$.+\$\s*$/.test(elem.textContent)) {
+			renderMath(elem, elem, false);
+		}
+	});
+});
